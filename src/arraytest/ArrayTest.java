9dd0799b6abe684b04fb3c21a6845639d858f152
rkaune <rkaune@7HPL6Y1.Student.UGDSB.ED>
2018-10-26 10:18:09 -0400

    Updated to ICS3U 2018

--- src/arraytest/ArrayTest.java
+++ src/arraytest/ArrayTest.java
@@ -3,27 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package arraytest;
-
 /**
  *
  * @author rkaune
  */
 public class ArrayTest {
-
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         String [] friends;
         friends = new String[5]; // fixed size of 5
-        String [] friendsTwo = {"Eric","Andrew","Evan Mitchell"};
-        friendsTwo[1]="Nick";
+        String [] friendsTwo = {"Peter","Evan","Aaron"};
+        friendsTwo[1]="Thomas";
         int [] ages = {17,15,17};
-//        for (int i=0;i<friendsTwo.length;i++){
-//            System.out.println(friendsTwo[i]);
-//        }
+        
+        for (int i=0;i<=friendsTwo.length;i++){
+            System.out.println(friendsTwo[i]);
+        }
         for (String element: friendsTwo){
             System.out.println(element);
         }
